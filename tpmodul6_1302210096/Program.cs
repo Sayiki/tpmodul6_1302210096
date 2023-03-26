4865e82480b248cf0c87ad45a6faf3902f817fb9
Sayiki <arzaqajradika1@gmail.com>
2023-03-26 14:13:24 +0700

    implementasi design by contract

--- tpmodul6_1302210096/Program.cs
+++ tpmodul6_1302210096/Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using tpmodul6_1302210096;
+class Program
+{
+    static void Main(string[] args)
+    {
+        SayaTubeVideo video1 = new SayaTubeVideo("Tutorial Design By Contract – Arzaq Ajradika");
+        SayaTubeVideo video2 = new SayaTubeVideo(null);
+        SayaTubeVideo video3 = new SayaTubeVideo("Tutorial Design By Contract - Arzaq Ajradika...............................................................................................................................................");
+
+        video1.IncreasePlayCount(500);
+
+
+        video2.IncreasePlayCount(20000000);
+        video3.IncreasePlayCount(-100);
+
+
+        Console.WriteLine("\n");
+
+        Console.WriteLine("Videos Details:\n");
+        video1.PrintVideoDetails();
+        Console.WriteLine("\n");
+        video2.PrintVideoDetails();
+        Console.WriteLine("\n");
+        video3.PrintVideoDetails();
+
+        Console.ReadLine();
+    }
+}
--- tpmodul6_1302210096/SayaTubeVideo.cs
+++ tpmodul6_1302210096/SayaTubeVideo.cs
@@ -1,36 +1,63 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Contracts;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace tpmodul6_1302210096
 {
+
     public class SayaTubeVideo
     {
-        int id;
-        string title;
-        int playCount;
-
+        private int id;
+        private string title;
+        private int playCount;
 
         public SayaTubeVideo(string title)
         {
-            this.title = title;
-            this.id = new Random().Next(10000, 99999);
-            this.playCount = 0;
+            try
+            {
+                if (string.IsNullOrEmpty(title) || title.Length > 100)
+                {
+                    throw new ArgumentException("Judul video harus memiliki panjang maksimal 100 karakter dan tidak boleh null atau kosong.");
+                }
+
+                this.id = new Random().Next(10000, 99999);
+                this.title = title;
+                this.playCount = 0;
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
-        public void IncreasePlayCount(int playCount)
-        {
-            this.playCount += playCount;
+        public void IncreasePlayCount(int count)
+        { 
+            try
+            {
+                checked
+                {
+                    if (count > 10000000 || count < 0)
+                    {
+                        throw new OverflowException("Play count tidak boleh lebih dari 10.000.000 atau kurang dari 0.");
+                    }
+
+                    playCount += count;
+                }
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
 
         public void PrintVideoDetails()
         {
-            Console.WriteLine("Video ID: " + this.id);
-            Console.WriteLine("Video title: " + this.title);
-            Console.WriteLine("Playcount: " + this.playCount);
-
+            Console.WriteLine("ID: " + this.id);
+            Console.WriteLine("Title: " + this.title);
+            Console.WriteLine("Play count: " + this.playCount);
         }
     }
 }

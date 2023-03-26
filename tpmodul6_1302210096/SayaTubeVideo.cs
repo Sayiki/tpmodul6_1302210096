052bb2cfe99743abf9af2383f0d1a036ac5c293e
Sayiki <arzaqajradika1@gmail.com>
2023-03-26 10:30:49 +0700

    menambahkan class SayaTubeVideo

---
+++ tpmodul6_1302210096/SayaTubeVideo.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tpmodul6_1302210096
+{
+    public class SayaTubeVideo
+    {
+        int id;
+        string title;
+        int playCount;
+
+
+        public SayaTubeVideo(string title)
+        {
+            this.title = title;
+            this.id = new Random().Next(10000, 99999);
+            this.playCount = 0;
+        }
+
+        public void IncreasePlayCount(int playCount)
+        {
+            this.playCount += playCount;
+        }
+
+        public void PrintVideoDetails()
+        {
+            Console.WriteLine("Video ID: " + this.id);
+            Console.WriteLine("Video title: " + this.title);
+            Console.WriteLine("Playcount: " + this.playCount);
+
+        }
+    }
+}
